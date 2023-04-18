b5b8f8718b2bc4f69065e62771ae86aa3c9258b2
Emirhan Koç <eemirhannk@gmail.com>
2023-04-18 06:58:20 +0300

    page connections and interface edits

--- app/src/main/java/com/example/yazlab2_2/Game.kt
+++ app/src/main/java/com/example/yazlab2_2/Game.kt
@@ -1,11 +1,33 @@
 package com.example.yazlab2_2
 
+import android.bluetooth.BluetoothA2dp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+
 
 class Game : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar()?.hide();
+
         setContentView(R.layout.game)
+
     }
+
+    var nextUp:Int = 0
+    var starting = 0
+    var num1 = 0
+    var num2 = 0
+    var num3 = 0
+    var num4 = 0
+    var shape_is = 0
+    var stop = 0
+    var lines = 0
+    var once = 0
+
+
 }
--- app/src/main/java/com/example/yazlab2_2/MainActivity.kt
+++ app/src/main/java/com/example/yazlab2_2/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.yazlab2_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar()?.hide();
+
+        startActivity(Intent(this,Game::class.java))
+
         setContentView(R.layout.activity_main)
+
 
         
     }
