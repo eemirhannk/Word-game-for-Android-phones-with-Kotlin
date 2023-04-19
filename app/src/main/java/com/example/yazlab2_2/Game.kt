bf6aa3c8083cb7907ecbba17748076781f08680c
Emirhan Koç <eemirhannk@gmail.com>
2023-04-19 09:58:09 +0300

    randomWord function created and interface edits

--- app/src/main/java/com/example/yazlab2_2/Game.kt
+++ app/src/main/java/com/example/yazlab2_2/Game.kt
@@ -1,11 +1,16 @@
 package com.example.yazlab2_2
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
-import android.widget.TextView
+import android.widget.Button
+import kotlinx.coroutines.delay
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 
 class Game : AppCompatActivity() {
@@ -32,11 +37,36 @@ class Game : AppCompatActivity() {
     var lines = 0
     var once = 0
 
-    private fun next_shape(){
-        findViewById<TextView>(R.id.B1).text = "D";findViewById<TextView>(R.id.B2).text = "D";findViewById<TextView>(R.id.B3).text = "D";findViewById<TextView>(R.id.B4).text = "D";findViewById<TextView>(R.id.B5).text = "D";findViewById<TextView>(R.id.B6).text = "D";findViewById<TextView>(R.id.B7).text = "D";findViewById<TextView>(R.id.B8).text = "D";
-        findViewById<TextView>(R.id.B9).text = "D";findViewById<TextView>(R.id.B10).text = "D";findViewById<TextView>(R.id.B11).text = "D";findViewById<TextView>(R.id.B12).text = "D";findViewById<TextView>(R.id.B13).text = "D";findViewById<TextView>(R.id.B14).text = "D";findViewById<TextView>(R.id.B15).text = "D";findViewById<TextView>(R.id.B16).text = "D";
-        findViewById<TextView>(R.id.B17).text = "D";findViewById<TextView>(R.id.B18).text = "D";findViewById<TextView>(R.id.B19).text = "D";findViewById<TextView>(R.id.B20).text = "D";findViewById<TextView>(R.id.B21).text = "D";findViewById<TextView>(R.id.B22).text = "D";findViewById<TextView>(R.id.B23).text = "D";findViewById<TextView>(R.id.B24).text = "D";
+    fun randomChar(): Char {
+        val chars = "ABCÇDEFGHIİJKLMNOÖPRSŞTUÜVYZ"
+        val index = (0..chars.length-1).random()
+        return chars[index]
+    }
 
+
+    private fun next_shape(){
+        findViewById<Button>(R.id.B1).text = randomChar().toString();findViewById<Button>(R.id.B2).text = randomChar().toString();findViewById<Button>(R.id.B3).text = randomChar().toString();findViewById<Button>(R.id.B4).text = randomChar().toString();findViewById<Button>(R.id.B5).text = randomChar().toString();findViewById<Button>(R.id.B6).text = randomChar().toString();findViewById<Button>(R.id.B7).text = randomChar().toString();findViewById<Button>(R.id.B8).text = randomChar().toString();
+        findViewById<Button>(R.id.B9).text = randomChar().toString();findViewById<Button>(R.id.B10).text = randomChar().toString();findViewById<Button>(R.id.B11).text = randomChar().toString();findViewById<Button>(R.id.B12).text = randomChar().toString();findViewById<Button>(R.id.B13).text = randomChar().toString();findViewById<Button>(R.id.B14).text = randomChar().toString();findViewById<Button>(R.id.B15).text = randomChar().toString();findViewById<Button>(R.id.B16).text = randomChar().toString();
+        findViewById<Button>(R.id.B17).text = randomChar().toString();findViewById<Button>(R.id.B18).text = randomChar().toString();findViewById<Button>(R.id.B19).text = randomChar().toString();findViewById<Button>(R.id.B20).text = randomChar().toString();findViewById<Button>(R.id.B21).text = randomChar().toString();findViewById<Button>(R.id.B22).text = randomChar().toString();findViewById<Button>(R.id.B23).text = randomChar().toString();findViewById<Button>(R.id.B24).text = randomChar().toString();
+        findViewById<Button>(R.id.B1).text = randomChar().toString();findViewById<Button>(R.id.B2).text = randomChar().toString();findViewById<Button>(R.id.B3).text = randomChar().toString();findViewById<Button>(R.id.B4).text = randomChar().toString();findViewById<Button>(R.id.B5).text = randomChar().toString();findViewById<Button>(R.id.B6).text = randomChar().toString();findViewById<Button>(R.id.B7).text = randomChar().toString();findViewById<Button>(R.id.B8).text = randomChar().toString();
+        findViewById<Button>(R.id.B9).text = randomChar().toString();findViewById<Button>(R.id.B10).text = randomChar().toString();findViewById<Button>(R.id.B11).text = randomChar().toString();findViewById<Button>(R.id.B12).text = randomChar().toString();findViewById<Button>(R.id.B13).text = randomChar().toString();findViewById<Button>(R.id.B14).text = randomChar().toString();findViewById<Button>(R.id.B15).text = randomChar().toString();findViewById<Button>(R.id.B16).text = randomChar().toString();
+        findViewById<Button>(R.id.B17).text = randomChar().toString();findViewById<Button>(R.id.B18).text = randomChar().toString();findViewById<Button>(R.id.B19).text = randomChar().toString();findViewById<Button>(R.id.B20).text = randomChar().toString();findViewById<Button>(R.id.B21).text = randomChar().toString();findViewById<Button>(R.id.B22).text = randomChar().toString();findViewById<Button>(R.id.B23).text = randomChar().toString();findViewById<Button>(R.id.B24).text = randomChar().toString();
+        findViewById<Button>(R.id.B1).text = randomChar().toString();findViewById<Button>(R.id.B2).text = randomChar().toString();findViewById<Button>(R.id.B3).text = randomChar().toString();findViewById<Button>(R.id.B4).text = randomChar().toString();findViewById<Button>(R.id.B5).text = randomChar().toString();findViewById<Button>(R.id.B6).text = randomChar().toString();findViewById<Button>(R.id.B7).text = randomChar().toString();findViewById<Button>(R.id.B8).text = randomChar().toString();
+        findViewById<Button>(R.id.B9).text = randomChar().toString();findViewById<Button>(R.id.B10).text = randomChar().toString();findViewById<Button>(R.id.B11).text = randomChar().toString();findViewById<Button>(R.id.B12).text = randomChar().toString();findViewById<Button>(R.id.B13).text = randomChar().toString();findViewById<Button>(R.id.B14).text = randomChar().toString();findViewById<Button>(R.id.B15).text = randomChar().toString();findViewById<Button>(R.id.B16).text = randomChar().toString();
+        findViewById<Button>(R.id.B17).text = randomChar().toString();findViewById<Button>(R.id.B18).text = randomChar().toString();findViewById<Button>(R.id.B19).text = randomChar().toString();findViewById<Button>(R.id.B20).text = randomChar().toString();findViewById<Button>(R.id.B21).text = randomChar().toString();findViewById<Button>(R.id.B22).text = randomChar().toString();findViewById<Button>(R.id.B23).text = randomChar().toString();findViewById<Button>(R.id.B24).text = randomChar().toString();
+
+
+        val random = Random.nextInt(1..8)
+        when(random){
+            1 -> {findViewById<Button>(R.id.B1).text = randomChar().toString()}
+            2 -> {findViewById<Button>(R.id.B2).text = randomChar().toString()}
+            3 -> {findViewById<Button>(R.id.B3).text = randomChar().toString()}
+            4 -> {findViewById<Button>(R.id.B4).text = randomChar().toString()}
+            5 -> {findViewById<Button>(R.id.B5).text = randomChar().toString()}
+            6 -> {findViewById<Button>(R.id.B6).text = randomChar().toString()}
+            7 -> {findViewById<Button>(R.id.B7).text = randomChar().toString()}
+            8 -> {findViewById<Button>(R.id.B8).text = randomChar().toString()}
+        }
 
     }
 
